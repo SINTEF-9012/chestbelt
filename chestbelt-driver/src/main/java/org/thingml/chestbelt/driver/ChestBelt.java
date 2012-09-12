65e22ff97890e92d1d1f5c89eba08c65686dcb5c
Franck Fleurey <franck@fleurey.com>
2012-09-12 18:04:51 +0200

    Fix

--- chestbelt-driver/src/main/java/org/thingml/chestbelt/driver/ChestBelt.java
+++ chestbelt-driver/src/main/java/org/thingml/chestbelt/driver/ChestBelt.java
@@ -19,8 +19,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class ChestBelt implements Runnable {
 
